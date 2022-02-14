464852d627551516477fee0d21b378fa47bac5fa
SofMaster <95130489+SofMaster@users.noreply.github.com>
2022-02-14 12:23:25 +0900

    add login

--- Step05_Final2/WebContent/WEB-INF/table.sql
+++ Step05_Final2/WebContent/WEB-INF/table.sql
@@ -4,6 +4,6 @@ CREATE TABLE users(
 	id VARCHAR2(100) PRIMARY KEY,
 	pwd VARCHAR2(100) NOT NULL,
 	email VARCHAR2(100),
-	profile VARCHAR2(100),
+	profile VARCHAR2(100), --프로필 이미지 경로를 저장할 칼럼
 	regdate Date
 );
